1ffde2f04b2e8f3e13d0ed2233a02c14e4bc2ed3
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2022-09-12 11:49:55 +0400

    add

---
+++ products.js
@@ -0,0 +1,22 @@
+const kgBtns = document.querySelectorAll(".kg-btn-single");
+const dropDown = document.querySelector("#drop-down-p")
+const more = document.querySelector("#more")
+const cart = document.querySelector(".green-box")
+const add = document.querySelector(".add-cart")
+const added = document.querySelector(".added-cart")
+
+
+kgBtns.forEach((kg) => {
+    kg.addEventListener('click', () => {
+      kg.classList.toggle('active');
+    });
+  });
+
+dropDown.addEventListener('click', () => {
+  more.classList.toggle('active');
+});
+
+cart.addEventListener('click', () => {
+  add.classList.toggle('active');
+  added.classList.toggle('active')
+});
