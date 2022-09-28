17cbdf1ad3eefedff4e65d8a8fe349ec72d60208
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2022-09-28 11:42:08 +0400

    add and change sliders

--- assets/app.js
+++ assets/app.js
@@ -26,39 +26,107 @@ function showSlides(n) {
 }
 
 
+let data= [
+  {
+      id: 1, 
+      imageUrl:'assets/images/3.jfif',
+      title: 'image title1',
+      url:'google.com'
+  },
+  {
+      id: 2, 
+      imageUrl:'assets/images/pedigri.png',
+      title: 'image title2',
+      url:'google.com'
+  },
+  {
+      id: 3, 
+      imageUrl:'assets/images/1.jfif',
+      title: 'image title3',
+      url:'google.com'
+  },
+  {
+      id: 4, 
+      imageUrl:'assets/images/2.jfif',
+      title: 'image title4',
+      url:'google.com'
+  },
 
-// brands
+]
 
 
-// let currentScrollposition = 0;
-// let scrollAmount = 320;
+let arrowLeft = document.getElementById('arrow-left-button');
+let arrowRight = document.getElementById('arrow-right-button');
+let sliderContent = document.getElementById('slider-content');
+
+let sliderIndex=0;
 
 
-// const sCont = document.querySelector(".storys-container");
-// const hScroll= document.querySelector(".horizontal-scroll");
-// const btnScrollleft = document.querySelector("#btn-scrol-left");
-// const btnScrollright = document.querySelector("#btn-scrol-right");
-// btnScrollleft.style.opacity = "0";
+function createAtag(item){
+  let tag =document.createElement('a');
+  tag.setAttribute('href',item.url);
+  tag.setAttribute('class', 'slide');
+
+  return tag;
+}
 
 
-// let maxScroll = -sCont.offsetwidth + hScroll.offsetwidth;
+function createImgtag(item){
+  let tagImage = document.createElement('img');
+  tagImage.setAttribute('src', item.imageUrl);
+  tagImage.setAttribute('alt',item.title);
 
 
-// function scrollyhorizontally(val){
-//   currentScrollposition += (val+scrollAmount);
-//   if(currentScrollposition > 0){
-//     currentScrollposition = 0
-//     btnScrollleft.style.opacity = "0";
-//   }else{
-//     btnScrollleft.style.opacity = "1";
-//   }
-//   if(currentScrollposition < maxScroll){
-//     currentScrollposition = maxScroll;
-//     btnScrollright.style.opacity = "0";
-//   }else{
-//     btnScrollright.style.opacity = "1";
-//   }
+ 
+  return tagImage;
+}
 
-//   sCont.style.left = currentScrollposition +"px";
 
-// }
+function createH2tag(item){
+  let tagtitle=document.createElement('h2');
+  tagtitle.setAttribute('class','title');
+  tagtitle.append(item.title);
+  
+
+
+  return tagtitle;
+
+}
+function setSlide(){
+  sliderContent.innerHTML= ' ';
+  let slideItem = createAtag(data[sliderIndex]);
+  let h2Tag = createH2tag(data[sliderIndex]);
+  let imgTag= createImgtag(data[sliderIndex]);
+
+  slideItem.appendChild(h2Tag);
+  slideItem.appendChild(imgTag);
+
+  sliderContent.appendChild(slideItem);
+
+
+
+  console.log(slideItem);
+}
+
+arrowLeft.addEventListener('click', function() {
+  if( sliderIndex <=0) {
+      return;
+
+  }
+
+  
+
+  sliderIndex--;
+  setSlide();
+});
+
+arrowRight.addEventListener('click', function() {
+
+  if (sliderIndex >= data.length - 1){
+      return
+  }
+  sliderIndex++;
+  setSlide();
+
+});
+setSlide();
